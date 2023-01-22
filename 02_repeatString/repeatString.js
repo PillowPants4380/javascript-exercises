e70d493b28053388cc84f701f2cf9ca502ea6fe1
David <felond4380@yahoo.com>
2023-01-22 12:15:16 -0500

    updated repeatString.js and passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+// const repeatString = function() {
+    
+   
+    
+//     return "heyheyhey";
+// };
 
-};
+function repeatString (string, numOfTimes){
+
+    let newString = "";
+
+    if(numOfTimes < 0){
+        return "ERROR";
+    }
+
+    for(let i = 0; i < numOfTimes; i++){
+        newString += string;
+    }
+
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
