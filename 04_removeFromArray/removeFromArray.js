7278510fb5d4078542b94ee98bb931fc23a25f1c
David <felond4380@yahoo.com>
2023-01-23 11:54:18 -0500

    updated removeFromArray.js and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = (startArray, ...Args) => {
+    const remArray = Array.from(Args);
+    let lstartArray = startArray.length;
+    const newArray = [];
+    for(let i = 0; i < lstartArray; i++){
+        if(!remArray.includes(startArray[i])){
+            newArray.push(startArray[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
