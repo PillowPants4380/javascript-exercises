778f6c587c47447b515279038fa24f993ff2e3df
David <felond4380@yahoo.com>
2023-01-23 15:22:35 -0500

    Updated sumAll.js and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
 
+    let startNum;
+    let endNum;
+    let sumAll = 0;
+
+    if(num1 < 0 || num2 < 0 || typeof(num1) ==="string" || typeof(num2) ==="string" || isNaN(num1) || isNaN(num2)){
+        return "ERROR";
+    }
+    num1 < num2 ? (startNum = num1, endNum = num2) : (startNum = num2, endNum = num1);
+
+    for(let i = startNum; i <= endNum; i++){
+        sumAll+=i;
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
